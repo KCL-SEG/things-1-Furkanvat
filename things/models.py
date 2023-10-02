4ffcece9df72f0c6e773d0cb59ac76da9aafb5d1
Furkanvat <furkan.hakan.vatansever@gmail.com>
2023-10-02 17:32:40 +0100

    Second Exercise

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
 # Create your models here.
+
+class Thing(models.Model):
+    name = models.CharField(max_length=255)  # Adjust the max_length as needed
+    description = models.CharField(max_length=255)  # Adjust the max_length as needed
+    quantity = models.IntegerField()
